848cd7c09de6885d70e5cd8452815fa1a396acf6
tongawz <gsosa@ciemsa.com.uy>
2025-04-17 01:30:54 -0300

    stylish-dimensions

--- PanelCutOptimizer/CMD.CutOptimizerTest/Program.cs
+++ PanelCutOptimizer/CMD.CutOptimizerTest/Program.cs
@@ -10,9 +10,9 @@ optimizer.AddPanel(panelx: 120, panely: 10, "D");
 optimizer.AddPanel(panelx: 110, panely: 20, "E");
 optimizer.AddPanel(panelx: 30, panely: 15, "F");
 optimizer.AddPanel(panelx: 20, panely: 100, "G");
-optimizer.AddPanel(panelx: 20, panely: 20, "H");
-optimizer.AddPanel(panelx: 10, panely: 25, "I");
-optimizer.AddPanel(panelx: 80, panely: 100, "J");
+//optimizer.AddPanel(panelx: 20, panely: 20, "H");
+//optimizer.AddPanel(panelx: 10, panely: 25, "I");
+//optimizer.AddPanel(panelx: 80, panely: 100, "J");
 //optimizer.AddPanel(panelx: 100, panely: 35, "K");
 
 
@@ -29,6 +29,8 @@ foreach (var panel in panels)
     dpi: 1000,
     marginBottom: 20,
     marginRight: 20,
+    marginTop: 35,
+    marginLeft: 35,
     scale: 5f,
     panelFontSize: 7f,
     tableFontSize: 6.5f);
--- PanelCutOptimizer/Lib.CutOptimizer/PanelDrawer.cs
+++ PanelCutOptimizer/Lib.CutOptimizer/PanelDrawer.cs
@@ -53,27 +53,10 @@ public static class PanelDrawer
     var placedPanelPen = new Pen(Color.Black, 3);
     var borderPen = new Pen(Color.Black, 1);
 
-    // Relleno del panel base
-    using (var hatchBrush = new HatchBrush(HatchStyle.ForwardDiagonal, Color.LightGray, Color.White))
-    {
-      var baseRect = new Rectangle(marginLeft, marginTop, panelWidthPx, panelHeightPx);
-      g.FillRectangle(hatchBrush, baseRect);
-    }
+    // Dibuja el panel base y sus cotas
+    DrawBasePanel(g, packedPanel, font, brush, basePanelPen, tableFont, borderPen, marginLeft, marginTop, panelWidthPx, panelHeightPx);
 
-    // Panel base
-    g.DrawRectangle(basePanelPen, marginLeft, marginTop, panelWidthPx, panelHeightPx);
-
-    // Cotas externas del panel base
-    DrawDimension(g, tableFont, brush, borderPen,
-      new Point(marginLeft, marginTop - 10),
-      new Point(marginLeft + panelWidthPx, marginTop - 10),
-      $"{packedPanel.Width}cm", isVertical: false);
-
-    DrawDimension(g, tableFont, brush, borderPen,
-      new Point(marginLeft - 10, marginTop),
-      new Point(marginLeft - 10, marginTop + panelHeightPx),
-      $"{packedPanel.Height}cm", isVertical: true);
-
+    // Paneles colocados
     foreach (var p in packedPanel.PlacedPanels)
     {
       var x = marginLeft + (int)(p.X * scale);
@@ -92,16 +75,18 @@ public static class PanelDrawer
 
       g.DrawString(name, font, brush, nameX, nameY);
 
-      // Cotas internas del panel recortado
-      DrawDimension(g, tableFont, brush, borderPen,
-        new Point(x, y - 6),
-        new Point(x + w, y - 6),
-        $"{p.Width}cm", isVertical: false);
 
-      DrawDimension(g, tableFont, brush, borderPen,
-        new Point(x - 6, y),
-        new Point(x - 6, y + h),
-        $"{p.Height}cm", isVertical: true);
+      // Cota horizontal interna (debajo del borde superior del panel)
+      DrawInternalPanelDimensions(g, tableFont, brush, borderPen,
+          new Point(x + 6, y + 6),  // Dentro del panel, arriba
+          new Point(x + w - 6, y + 6),
+          $"{p.Width}cm");
+
+      // Cota vertical interna (a la derecha del borde izquierdo del panel)
+      DrawInternalPanelDimensions(g, tableFont, brush, borderPen,
+          new Point(x + 6, y + 6),
+          new Point(x + 6, y + h - 6),
+          $"{p.Height}cm", isVertical: true);
     }
 
     // Tabla
@@ -144,35 +129,195 @@ public static class PanelDrawer
     bmp.Save(outputPath, ImageFormat.Png);
   }
 
-  private static void DrawDimension(Graphics g, Font font, Brush brush, Pen pen,
-    Point start, Point end, string text, bool isVertical)
+  private static void DrawBasePanel(
+      Graphics g,
+      PackedBasePanel packedPanel,
+      Font font,
+      Brush brush,
+      Pen basePanelPen,
+      Font tableFont,
+      Pen borderPen,
+      int marginLeft,
+      int marginTop,
+      int panelWidthPx,
+      int panelHeightPx)
   {
+    // Relleno del panel base
+    using (var hatchBrush = new HatchBrush(HatchStyle.ForwardDiagonal, Color.LightGray, Color.White))
+    {
+      var baseRect = new Rectangle(marginLeft, marginTop, panelWidthPx, panelHeightPx);
+      g.FillRectangle(hatchBrush, baseRect);
+    }
+
+    // Panel base
+    g.DrawRectangle(basePanelPen, marginLeft, marginTop, panelWidthPx, panelHeightPx);
+
+    // Cotas externas del panel base
+    DrawBasePanelDimensions(g, tableFont, brush, borderPen,
+        new Point(marginLeft, marginTop - 6),    // Separación ajustada
+        new Point(marginLeft + panelWidthPx, marginTop - 6),
+        $"{packedPanel.Width}cm");
+
+    DrawBasePanelDimensions(g, tableFont, brush, borderPen,
+        new Point(marginLeft - 6, marginTop),    // Separación ajustada
+        new Point(marginLeft - 6, marginTop + panelHeightPx),
+        $"{packedPanel.Height}cm", isVertical: true);
+  }
+
+  private static void DrawBasePanelDimensions(
+    Graphics g, Font font, Brush brush, Pen pen,
+    Point start, Point end, string text, bool isVertical = false)
+  {
+    // Separar la línea de dimensión del borde 1 píxel
+    if (isVertical)
+    {
+      start = new Point(start.X - 1, start.Y);
+      end = new Point(end.X - 1, end.Y);
+    }
+    else
+    {
+      start = new Point(start.X, start.Y - 1);
+      end = new Point(end.X, end.Y - 1);
+    }
+
     g.DrawLine(pen, start, end);
 
     var arrowSize = 4;
     if (isVertical)
     {
+      // Flechas
       g.DrawLine(pen, start, new Point(start.X - arrowSize, start.Y + arrowSize));
       g.DrawLine(pen, start, new Point(start.X + arrowSize, start.Y + arrowSize));
       g.DrawLine(pen, end, new Point(end.X - arrowSize, end.Y - arrowSize));
       g.DrawLine(pen, end, new Point(end.X + arrowSize, end.Y - arrowSize));
+
+      var textSize = g.MeasureString(text, font);
+
+      // Punto medio de la línea vertical
+      float centerX = start.X;
+      float centerY = (start.Y + end.Y) / 2;
+
+      // Guardar estado gráfico actual
+      GraphicsState state = g.Save();
+
+      // Trasladar al centro y rotar -90°
+      g.TranslateTransform(centerX, centerY);
+      g.RotateTransform(-90);
+
+      // Posicionar texto (centrado)
+      float textX = -textSize.Width / 2;
+      float textY = -textSize.Height / 2;
+
+      float padding = 2;
+      var backgroundRect = new RectangleF(
+          textX - padding,
+          textY - padding,
+          textSize.Width + 2 * padding,
+          textSize.Height + 2 * padding);
+
+      // Fondo blanco y texto
+      g.FillRectangle(Brushes.White, backgroundRect);
+      g.DrawString(text, font, brush, textX, textY);
+
+      // Restaurar estado gráfico
+      g.Restore(state);
     }
     else
     {
+      // Flechas
       g.DrawLine(pen, start, new Point(start.X + arrowSize, start.Y - arrowSize));
       g.DrawLine(pen, start, new Point(start.X + arrowSize, start.Y + arrowSize));
       g.DrawLine(pen, end, new Point(end.X - arrowSize, end.Y - arrowSize));
       g.DrawLine(pen, end, new Point(end.X - arrowSize, end.Y + arrowSize));
+
+      var textSize = g.MeasureString(text, font);
+
+      float textX = (start.X + end.X) / 2 - textSize.Width / 2;
+      float textY = (start.Y + end.Y) / 2 - textSize.Height / 2;
+
+      float padding = 2;
+      var backgroundRect = new RectangleF(
+          textX - padding,
+          textY - padding,
+          textSize.Width + 2 * padding,
+          textSize.Height + 2 * padding);
+
+      g.FillRectangle(Brushes.White, backgroundRect);
+      g.DrawString(text, font, brush, textX, textY);
     }
+  }
 
-    var textSize = g.MeasureString(text, font);
-    float textX = isVertical
-        ? start.X - textSize.Width - 4
-        : (start.X + end.X) / 2 - textSize.Width / 2;
-    float textY = isVertical
-        ? (start.Y + end.Y) / 2 - textSize.Height / 2
-        : start.Y - textSize.Height - 2;
 
-    g.DrawString(text, font, brush, textX, textY);
+
+  private static void DrawInternalPanelDimensions(
+    Graphics g, Font font, Brush brush, Pen pen,
+    Point start, Point end, string text, bool isVertical = false)
+  {
+    var arrowSize = 4;
+
+    if (isVertical)
+    {
+      // Separar línea 1px a la derecha
+      start = new Point(start.X + 3, start.Y + 1);
+      end = new Point(end.X + 3, end.Y);
+
+      g.DrawLine(pen, start, end);
+
+      // Flechas verticales
+      g.DrawLine(pen, start, new Point(start.X - arrowSize, start.Y + arrowSize));
+      g.DrawLine(pen, start, new Point(start.X + arrowSize, start.Y + arrowSize));
+      g.DrawLine(pen, end, new Point(end.X - arrowSize, end.Y - arrowSize));
+      g.DrawLine(pen, end, new Point(end.X + arrowSize, end.Y - arrowSize));
+
+      var textSize = g.MeasureString(text, font);
+      float centerX = start.X;
+      float centerY = (start.Y + end.Y) / 2;
+
+      GraphicsState state = g.Save();
+      g.TranslateTransform(centerX, centerY);
+      g.RotateTransform(-90);
+
+      float textX = -textSize.Width / 2;
+      float textY = -textSize.Height / 2;
+
+      float padding = 2;
+      var backgroundRect = new RectangleF(
+          textX - padding,
+          textY - padding,
+          textSize.Width + 2 * padding,
+          textSize.Height + 2 * padding);
+
+      g.FillRectangle(Brushes.White, backgroundRect);
+      g.DrawString(text, font, brush, textX, textY);
+
+      g.Restore(state);
+    }
+    else
+    {
+      start = new Point(start.X + 3, start.Y + 2);
+      end = new Point(end.X, end.Y + 2);
+
+      // Horizontal sin desplazamiento
+      g.DrawLine(pen, start, end);
+
+      g.DrawLine(pen, start, new Point(start.X + arrowSize, start.Y - arrowSize));
+      g.DrawLine(pen, start, new Point(start.X + arrowSize, start.Y + arrowSize));
+      g.DrawLine(pen, end, new Point(end.X - arrowSize, end.Y - arrowSize));
+      g.DrawLine(pen, end, new Point(end.X - arrowSize, end.Y + arrowSize));
+
+      var textSize = g.MeasureString(text, font);
+      float textX = (start.X + end.X) / 2 - textSize.Width / 2;
+      float textY = start.Y - 4;
+
+      float padding = 2;
+      var backgroundRect = new RectangleF(
+          textX - padding,
+          textY - padding,
+          textSize.Width + 1 * padding,
+          textSize.Height + 1 * padding);
+
+      g.FillRectangle(Brushes.White, backgroundRect);
+      g.DrawString(text, font, brush, textX, textY);
+    }
   }
 }
